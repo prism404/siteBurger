a2fc639d3dba4475b506bab8e5001aa577faecc5
Eugenius <pro.maximilien@protonmail.com>
2023-05-30 12:03:03 +0200

    Include WebAssembly

--- wasm_src/wasm.d
+++ wasm_src/wasm.d
@@ -1,13 +1,18 @@
+import ldc.attributes;
+
 extern(C): // disable D mangling
 
-// import a function "callback" from default import module name "env"
-void callback(double a, double b, double c);
+// import a function "add" from module name "math" and rename it to "add_numbers"
+@llvmAttr("wasm-import-module", "math") @llvmAttr("wasm-import-name", "add") { 
+	int add_numbers(int a, int b);
+}
 
-double add(double a, double b)
+// export a function "hello"
+export int hello(int a, int b, int c)
 {
-    const c = a + b;
-    callback(a, b, c);
-    return c;
+	int s1 = add_numbers(a, b);
+	int s2 = add_numbers(s1, c);
+	return s2;
 }
 
 void _start() {}
